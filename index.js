1ffdbb6cb81c0954be57be018133958ad59c7abb
Mikko Yliniemi <mikessu@iki.fi>
2018-03-02 10:57:36 +0200

    eslint added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express')
- 
-var app = express()
- 
-app.get('/notes', function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
-})
- 
-app.listen(3000)
+const express = require('express');
+
+const app = express();
+
+app.get('/notes', (req, res) => {
+  res.json({ notes: 'This is your notebook. Edit this to start saving your notes!' });
+});
+
+app.listen(3000);
