342196557c5ad079f86339db5ee92322145d8bc7
Mikko Yliniemi <mikessu@iki.fi>
2018-03-02 11:08:45 +0200

    added plan

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/notes', (req, res) => {
-  res.json({ notes: 'This is your notebook. Edit this to start saving your notes!' });
+app.get('/', (req, res) => {
+  res.json({ notes: 'Test' });
 });
 
 app.listen(3000);
