bf6ed3fbf6da34c022dc6fa662a3ea335d1c3046
Mikko Yliniemi <mikessu@iki.fi>
2018-03-05 12:41:10 +0200

    removed options.js

--- options.js
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/questions/22348705/best-way-to-store-db-config-in-node-js-express-app
-const fs = require('fs');
-
-const configPath = './config.json';
-
-const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
-exports.storageConfig = parsed;
