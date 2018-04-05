fcd3ec5c0a1941543a4d3088f15aa6ab4e8ea9b8
Mikko Yliniemi <mikessu@iki.fi>
2018-04-05 13:33:31 +0300

    secure mysql connection

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
 const config = require('./config/config');
+const fs = require('fs');
 
 module.exports = {
 
@@ -24,6 +25,11 @@ module.exports = {
       database: config.mysql_database,
       user: config.mysql_user,
       password: config.mysql_pass,
+      ssl: {
+        // DO NOT DO THIS
+        // set up your ca correctly to trust the connection
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 2,
